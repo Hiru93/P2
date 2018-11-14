263dc50b2d3b0d29a84eed0513dfc584990cd157
tia <mattia.gallinaro93@gmail.com>
2018-11-14 16:35:06 +0100

    .

---
+++ Orario/telefonata.cpp
@@ -0,0 +1,6 @@
+#include "telefonata.h"
+
+Telefonata::Telefonata()
+{
+
+}
---
+++ Orario/telefonata.h
@@ -0,0 +1,11 @@
+#ifndef TELEFONATA_H
+#define TELEFONATA_H
+
+
+class Telefonata
+{
+public:
+    Telefonata();
+};
+
+#endif // TELEFONATA_H
