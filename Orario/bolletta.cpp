71d7036cffabc4c68d75529d6d090f2926f2187a
tia <mattia.gallinaro93@gmail.com>
2018-11-20 16:39:07 +0100

    .

---
+++ Orario/bolletta.cpp
@@ -0,0 +1,6 @@
+#include "bolletta.h"
+
+bolletta::bolletta()
+{
+
+}
---
+++ Orario/bolletta.h
@@ -0,0 +1,11 @@
+#ifndef BOLLETTA_H
+#define BOLLETTA_H
+
+
+class bolletta
+{
+public:
+    bolletta();
+};
+
+#endif // BOLLETTA_H
